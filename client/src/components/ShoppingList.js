6a242433df9ce20b50d0eb4922f40aaafb438f0d
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-27 00:00:32 +0530

    Redux state management implemented

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -2,18 +2,26 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem, Container, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import uuid from "uuid";
+import { connect } from "react-redux";
+import { getItems } from "../actions/itemActions";
+import { PropTypes } from "prop-types";
 
-export default class ShoppingList extends Component {
-    state = {
-        items: [
-            { id: uuid(), name: "Eggs" },
-            { id: uuid(), name: "Milk" },
-            { id: uuid(), name: "Water" },
-            { id: uuid(), name: "Chicken" }
-        ]
-    };
+class ShoppingList extends Component {
+    // state = {
+    //     items: [
+    //         { id: uuid(), name: "Eggs" },
+    //         { id: uuid(), name: "Milk" },
+    //         { id: uuid(), name: "Water" },
+    //         { id: uuid(), name: "Chicken" }
+    //     ]
+    // };
+
+    componentDidMount() {
+        this.props.getItems();
+    }
+    
     render() {
-        const { items } = this.state;
+        const { items } = this.props.item;
         return (
             <Container>
                 <Button
@@ -63,3 +71,17 @@ export default class ShoppingList extends Component {
         );
     }
 }
+
+ShoppingList.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    item: state.item
+});
+
+export default connect(
+    mapStateToProps,
+    { getItems }
+)(ShoppingList);
