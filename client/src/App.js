84dbae033a3ffaabb3e0fc070fd55fc59159fd42
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-26 21:57:59 +0530

    Navbar component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import AppNavbar from "./components/AppNavbar";
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Hello</h1>
-        </div>
-    );
+export default class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <AppNavbar/>
+            </div>
+        );
+    }
 }
-
-export default App;
