857aee17b96bb5d2c0b83973d11b09dc55d32a2c
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-27 22:20:05 +0530

    Making app production ready

--- server.js
+++ server.js
@@ -22,8 +22,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 // Use routes
-
 app.use("/api/items", require("./routes/api/items"));
+
+// Serve static assets if app is in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
